c2a6f8b33413c250c10d67a070e9235fd0f6850a
yuanchao@egova.com.cn <yuanchao@egova.com.cn>
2019-11-19 23:33:00 +0800

    Updates

---
+++ precache-manifest.cd44fa4ab975fcf384fa17244f1079b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dcf8868efe98b39103f942800f1988",
+    "url": "/accounts-app/index.html"
+  },
+  {
+    "revision": "c65d006905d7a9cacf80",
+    "url": "/accounts-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d553061a825a841ab7d7",
+    "url": "/accounts-app/static/js/2.3ea1658d.chunk.js"
+  },
+  {
+    "revision": "c65d006905d7a9cacf80",
+    "url": "/accounts-app/static/js/main.346e1264.chunk.js"
+  },
+  {
+    "revision": "229a5083776ecf9bded7",
+    "url": "/accounts-app/static/js/runtime-main.3207a37c.js"
+  }
+]);
